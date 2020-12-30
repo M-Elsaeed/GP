b524d415867bd60a81106548cb5a7fae062c7648
Mohammed Ehab <m.elsaeed1998@gmail.com>
2020-12-30 15:10:14 +0200

    Combined cutting frames options into one script

--- scripts/cutvid.py
+++ scripts/cutvid.py
@@ -1,6 +1,9 @@
 from cv2 import cv2
 import os
 
+LOWRES_OUTPUT = False
+GREYSCALE_OUTPUT = False
+
 folderPath = input(
     "Please Create a folder called 'frames_color' then\nEnter the ABSOLUTE Path to the Folder of videos\n"
 )
@@ -19,6 +22,14 @@ for i in vidCaps:
     success, image = i.read()
     while success:
         image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-        cv2.imwrite(f"./frames_color/frame{count}.jpg", image)
+        
+        if GREYSCALE_OUTPUT:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        if LOWRES_OUTPUT:
+            cv2.imwrite(f"./frames_color/frame{count}.jpg", image, [cv2.IMWRITE_JPEG_QUALITY,50])
+        else:
+            cv2.imwrite(f"./frames_color/frame{count}.jpg", image)
+        
         success, image = i.read()
         count += 1
