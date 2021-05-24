8ee4c6f16f679ffb33ef1191884ce5652f4deee3
Mohammed Ehab ElSaeed <m.elsaeed1998@gmail.com>
2021-05-24 21:30:26 +0200

    Printing Label in addition to probabilities

--- scripts/inference.py
+++ scripts/inference.py
@@ -9,7 +9,6 @@ gpus = tf.config.experimental.list_physical_devices(device_type="GPU")
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
-# model = keras.models.load_model(f"./trainedmodel.h5")
 model = keras.models.load_model(f"./color_normalized_nodropout.h5")
 
 
@@ -24,7 +23,17 @@ while(True):
         cv2.imshow('output',frame)
         frame = frame / 255
         x_to_predict = np.array([cv2.resize(frame, dsize=(270, 480))])
-        print(model.predict(x_to_predict))
+        # print()
+        yHat = model.predict(x_to_predict)[0]
+        # print(yHat)
+        # print(len(yHat))
+        if yHat[0] > yHat[1] and yHat[0] > yHat[2]:
+            print(yHat, "L")
+        elif yHat[1] > yHat[0] and yHat[1] > yHat[2]:
+            print(yHat, "R")
+        else:
+            print(yHat, "F")
+    
     q = cv2.waitKey(1)
     if q == ord("q"):
         break
