ff6e1c774e85ca20f541dae4cea5f22b30cb1f84
Mohammed Ehab ElSaeed <m.elsaeed1998@gmail.com>
2021-07-10 19:57:02 +0200

    Refactoring

--- inference.py
+++ inference.py
@@ -4,80 +4,50 @@ import cv2
 import silence_tensorflow.auto  # pylint: disable=unused-import
 import tensorflow as tf
 from tensorflow import keras
+from utilities import cropFaceDNN, cropFaceHaarCascade
 
 gpus = tf.config.experimental.list_physical_devices(device_type="GPU")
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
 # model = keras.models.load_model(f"./trainedModels/blkshirts2.h5")
-model = keras.models.load_model(f"./trainedModels/croppedModelTOP.h5")
+model = keras.models.load_model(f"./trainedModels/croppedModelFC.h5")
 
 
 
-# url = 'http://192.168.1.89:8080/video'
-url = 'http://172.20.10.2:8080/video'
-url = 'http://192.168.0.104:8080/video'
+# url = 0 for camera
+url = 'http://192.168.1.102:8080/video'
 
 cap = cv2.VideoCapture(url)
 # cap = cv2.VideoCapture("../test/right_test.mp4")
-
-def face_detector(img):
-
-    face_cascade = cv2.CascadeClassifier(
-        cv2.data.haarcascades + "haarcascade_frontalface_alt.xml"
-    )
-    # load color (BGR) image
-    # img = cv2.imread(img_path)
-    # convert BGR image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # find faces in image
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-    # print number of faces detected in the image
-    # print("Number of faces detected:", len(faces))
-
-    # get bounding box for each detected face
-    for (x, y, w, h) in faces:
-        # add bounding box to color image
-        # cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        crop_img = img[y : y + h, x : x + w]
-        crop_img = cv2.resize(crop_img, (224, 224))
-        # print(crop_img.shape)
-        # crop_img_gray = gray[x:x+w, y:y+h]
-        # align_Face(crop_img, crop_img_gray)
-        # cv2.waitKey(0)
-
-    # convert BGR image to RGB for plotting
-    # cv_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # img = cv2.resize(cv_rgb, (480, 480))
-    if len(faces):
-        # cv2.imshow('cropped', crop_img)
-        # cv2.waitKey(50)
-        # print(crop_img.shape)
-        return crop_img
-    return None
+def cautiousImshow(name, image):
+    try:
+        cv2.imshow(name, image)
+    except:
+        print("image invalid")
 
 cv2.namedWindow("output", cv2.WINDOW_NORMAL)
 while(True):
     ret, frame = cap.read()
     # frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
     if frame is not None:
-        frame = face_detector(frame)
+        # cv2.imshow('feed',frame)
+        frame = cropFaceDNN(frame)
         if frame is not None:
-            cv2.imshow('output',frame)
             x_to_predict = np.array([cv2.resize(frame, dsize=(224, 224))]).astype(np.float32)
             x_to_predict = x_to_predict / 255.0
             # print()
             yHat = model.predict(x_to_predict)[0]
-            print(yHat, end=" ")
+            prediction = None
+            # print(yHat, end=" ")
             # print(len(yHat))
             if yHat[0] > yHat[1] and yHat[0] > yHat[2]:
-                print("L")
+                prediction="L"
             elif yHat[1] > yHat[0] and yHat[1] > yHat[2]:
-                print("R")
+                prediction="R"
             else:
-                print("F")
+                prediction="F"
+            cautiousImshow('output',cv2.putText(frame, prediction, (50, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2))
     
     q = cv2.waitKey(1)
     if q == ord("q"):
--- utilities.py
+++ utilities.py
@@ -1,2 +1,54 @@
-def viewImagesFromNpy(path):
-    
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNetFromCaffe(
+    "./faceDetectionModel/deploy.prototxt",
+    "./faceDetectionModel/res10_300x300_ssd_iter_140000.caffemodel",
+)
+def cropFaceDNN(image):
+    # print(image.dtype, image.shape)
+    crop_img = None
+
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(image, (300, 300)),
+        1.0,
+        (300, 300),
+        (104.0, 177.0, 123.0)
+    )
+    net.setInput(blob)
+    detections = net.forward()
+    i = 0
+    # print("faces", detections.shape[2])
+    while i < detections.shape[2]:
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.95:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            # print(startY, endY, startX, endX)
+            if startY <= image.shape[0]/2 and startY >=0 and endY >=0 and startX >=0 and endX >=0 :
+                crop_img = image[startY:endY, startX:endX]
+                try:
+                    crop_img = cv2.resize(crop_img, (224, 224))
+                except:
+                    print(startY, endY, startX, endX)
+        i += 1
+    return crop_img
+
+def cropFaceHaarCascade(image):
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_alt.xml"
+    )
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        crop_img = image[y : y + h, x : x + w]
+        crop_img = cv2.resize(crop_img, (224, 224))
+    if len(faces):
+        return crop_img
+    return None
+
+def printModelDetails(modelPath):
+    import keras
+    model = keras.models.load_model(modelPath)
+    model.summary()
